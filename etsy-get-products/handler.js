9c44a7e0dc22d0cfe57a2f6a1f3bcb1ce745bd7d
Sy <sybuena@gmail.com>
2022-09-21 06:19:50 +0800

    serverless

--- etsy-get-products/handler.js
+++ etsy-get-products/handler.js
@@ -1,5 +1,7 @@
 'use strict';
 var axios = require('axios');
+var qs = require('qs');
+const { access } = require('fs');
 const mysql = require('serverless-mysql')({
 	config: {
 		host     : process.env.DB_HOST,
@@ -16,7 +18,6 @@ module.exports.get = async (event, context) => {
 
 	let encryptor = new Encryptor({key: process.env.LARAVEL_API_KEY});
 	
-
 	//return error if no store_id
 	if(typeof event.store_id === 'undefined') {
 		return {
@@ -55,34 +56,44 @@ module.exports.get = async (event, context) => {
 
 	// Run clean up function
 	await mysql.end();
+	var shopId = integration[0].etsy_shop_id;
+	console.log(integration); 
+	//return false;
 	// var params = JSON.parse(event);
 	//decryt access_token
-	var access_token = encryptor.decrypt(integration[0].access_token);
-	var storeUrl = store[0].url;
-	var url = storeUrl+'/admin/api/2022-07/products.json';
-	// console.log(storeUrl)
-	// console.log(access_token)
+	//console.log(integration[0].access_token);
+	var clientId = process.env.ETSY_API_KEY;
+	var accessToken = encryptor.decrypt(integration[0].access_token);
+	var refreshToken = encryptor.decrypt(integration[0].refresh_token);
+	//1st step is renew the access_token using refresh_token
+	var newToken = await requestNewToken(refreshToken, clientId);
+
+	console.log(newToken);
+	
+	var url = 'https://openapi.etsy.com/v3/application/shops/'+shopId+'/listings?includes=Inventory,Images';
+
+	
+	var headers = { 
+		'Content-Type': 'application/json', 
+		'Authorization': 'Bearer '+newToken.access_token, 
+		'x-api-key' : clientId
+	};
+	
 	//add filter on getting products based on updated date so we dont need to pull everything
 	await axios({
 		method: 'get',
 		url: url,
-		headers: { 
-		  'Content-Type': 'application/json', 
-		  'X-Shopify-Access-Token': access_token, 
-		}
+		headers: headers
 	}).then(async function (response) {
 		console.log(response.data)
-		//we need to call bagisto now to save the data
+		//we need to call bagisto now to save the data together with new token
 		//console.log(JSON.stringify(response.data));
 		await saveRaw(response.data, store[0].id, store[0].customer_id)
 	}).catch(function (error) {
 		console.log(error);
 	});
-
-
-	 // Return the results
-	 //return results
-
+	
+	//return results
 	return {
 		statusCode: 200,
 		body: JSON.stringify(
@@ -97,9 +108,37 @@ module.exports.get = async (event, context) => {
 	};
 };
 
+async function requestNewToken(refreshToken, clientId) {
+	
+	var url = 'https://api.etsy.com/v3/public/oauth/token';
+	// console.log(storeUrl)
+	// console.log(access_token)
+	var data = qs.stringify({
+		'grant_type': 'refresh_token',
+		'client_id': clientId,
+		'refresh_token': refreshToken 
+	});
+	
+	//add filter on getting products based on updated date so we dont need to pull everything
+	return axios({
+		method: 'post',
+		url: url,
+		headers: { 
+		  'Content-Type': 'application/x-www-form-urlencoded', 
+		},
+		data : data
+	}).then(async function (response) {
+		console.log('WTF', response.data)
+		return response.data;
+	}).catch(function (error) {
+		console.log(error);
+		return error
+	});
+}
+
 async function saveRaw(products, storeId, customerId) {
 	
-	var url = 'http://127.0.0.1:8000/serverless-api/shopify/saveRaw';
+	var url = 'http://127.0.0.1:8000/serverless-api/etsy/saveRaw';
 	var data = JSON.stringify({
 		'products': products.products,
 		'store_id' : storeId,
